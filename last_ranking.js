135482524013e5236ff697f6cd25d3c5ebf47b29
HorpioM <149604841+HorpioM@users.noreply.github.com>
2023-11-06 12:59:28 +0100

    Update latest ranking in latest_ranking.js

--- last_ranking.js
+++ last_ranking.js
@@ -1,24 +1,5 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const rankingsList = document.getElementById("rankings-list");
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-05_09_13_30.html">corporaciones_2023-11-05_09_13_30</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-04_13_27_35.html">corporaciones_2023-11-04_13_27_35</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-04_10_53_24.html">corporaciones_2023-11-04_10_53_24</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-04_08_39_46.html">corporaciones_2023-11-04_08_39_46</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-04_07_39_29.html">corporaciones_2023-11-04_07_39_29</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-04_03_27_21.html">corporaciones_2023-11-04_03_27_21</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-04_00_55_23.html">corporaciones_2023-11-04_00_55_23</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-03_19_21_21.html">corporaciones_2023-11-03_19_21_21</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-02_12_56_14.html">corporaciones_2023-11-02_12_56_14</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-02_12_54_51.html">corporaciones_2023-11-02_12_54_51</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-02_05_16_41.html">corporaciones_2023-11-02_05_16_41</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-02_03_51_31.html">corporaciones_2023-11-02_03_51_31</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-02_02_21_27.html">corporaciones_2023-11-02_02_21_27</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-01_20_41_23.html">corporaciones_2023-11-01_20_41_23</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-01_20_27_36.html">corporaciones_2023-11-01_20_27_36</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-01_19_36_44.html">corporaciones_2023-11-01_19_36_44</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-01_19_26_11.html">corporaciones_2023-11-01_19_26_11</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-01_19_22_55.html">corporaciones_2023-11-01_19_22_55</a></li>`);
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-01_19_05_28.html">corporaciones_2023-11-01_19_05_28</a></li>`);
-
+    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2023-11-06_12_59_07.html">corporaciones_2023-11-06_12_59_07</a></li>`);
 });
