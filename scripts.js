e8c2b63ddcc90d51a0f95b25cc71146902980727
HorpioM <149604841+HorpioM@users.noreply.github.com>
2024-04-20 02:49:28 +0200

    Update rankings list in script.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const rankingsList = document.getElementById("rankings-list");
+    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2024-04-20_02_49_24.html">corporaciones_2024-04-20_02_49_24</a></li>`);
     rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2024-04-19_20_18_08.html">corporaciones_2024-04-19_20_18_08</a></li>`);
     rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2024-03-29_21_20_18.html">corporaciones_2024-03-29_21_20_18</a></li>`);
     rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2024-02-03_07_32_23.html">corporaciones_2024-02-03_07_32_23</a></li>`);
