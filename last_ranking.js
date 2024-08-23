1e2d88059bc654b07247fae2262b5b51549189d9
HorpioM <149604841+HorpioM@users.noreply.github.com>
2024-08-23 15:18:48 +0200

    Update latest ranking in latest_ranking.js

--- last_ranking.js
+++ last_ranking.js
@@ -1,5 +1,5 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const rankingsList = document.getElementById("rankings-list");
-    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2024-08-21_06_48_34.html">corporaciones_2024-08-21_06_48_34</a></li>`);
+    rankingsList.insertAdjacentHTML("beforeend", `<li><a href="Ranking/corporaciones_2024-08-23_15_18_42.html">corporaciones_2024-08-23_15_18_42</a></li>`);
 });
